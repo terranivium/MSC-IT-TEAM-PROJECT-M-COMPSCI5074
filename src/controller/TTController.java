59d15f6f928b6193cf82d594f495f0b910c1b175
mantonmilne <59877506+mantonmilne@users.noreply.github.com>
2020-02-03 01:14:14 +0000

    Database implemented + some changes&bug fix in ttmodel
    Database now updates with real game stats. Request stats now ready to be called by menu selection. Bug fix in TTModel - new method ensures new games start with zeroes stats and empty hands. (all testing done with only 1 ai opponent)

--- src/controller/TTController.java
+++ src/controller/TTController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.util.Scanner;
 
+import database_testlog.DatabaseInteractor;
 import model.Player;
 import model.TTModel;
 import view.TTCLIView;
@@ -12,11 +13,13 @@ public class TTController {
 	private TTCLIView view; // view instance
 	private Scanner systemInput = new Scanner(System.in); // User input instance
 	private int readInput; // Holds user input for condition checks
-
+	private DatabaseInteractor dbI = new DatabaseInteractor(); //instance of connector to database storing game statistics
+	
+	
 	public TTController(TTModel model, TTCLIView view) {
 		this.model = model;
 		this.view = view;
-	}
+		}
 
 	// Menu loop
 	public void runtimeMenu() {
@@ -78,6 +81,9 @@ public class TTController {
 			this.model.compareCards(this.readInput);
 		} 
 		this.view.gameWinner();
+		dbI.updateDb(this.model.getGameWinner(), this.model.getNumOfDraws(), this.model.getNumOfRounds(), this.model.getAllWonRounds());//calls on model methods to supply arguments to dbI for updating db.
+		dbI.dbRequest(); //testline - delete
+		this.model.setNewGameStates();
 		this.runtimeMenu();
 	}
 
